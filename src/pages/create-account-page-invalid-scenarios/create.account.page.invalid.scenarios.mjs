ec04f91ff13eec9b3c9724766fe5463b7e8323ec
RomanSibakovskij <benutzer373@gmail.com>
2025-10-29 08:13:48 +0200

    Add relevant web elements for CreateAccountPageInvalidScenarios utility sheet

--- src/pages/create-account-page-invalid-scenarios/create.account.page.invalid.scenarios.mjs
+++ src/pages/create-account-page-invalid-scenarios/create.account.page.invalid.scenarios.mjs
@@ -6,6 +6,13 @@ class CreateAccountPageInvalidScenarios extends BasePage{
 
     constructor(page) {
         super(page);
+
+        //relevant web elements
+        this._signUpPageFirstNameInputField = page.locator("//input[@id='firstname']");
+        this._signUpPageLastNameInputField = page.locator("//input[@id='lastname']");
+        this._signUpPageEmailInputField = page.locator("//input[@id='email']");
+        this._signUpPagePasswordInputField = page.locator("//input[@id='password']");
+
     }
 
 }
