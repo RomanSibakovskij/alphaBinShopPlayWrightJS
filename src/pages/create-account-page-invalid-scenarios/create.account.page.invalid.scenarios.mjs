3830013668b73687fc3ecf90942e03dbeb2ed2f9
RomanSibakovskij <benutzer373@gmail.com>
2025-10-29 08:12:56 +0200

    Add empty CreateAccountPageInvalidScenarios utility sheet

---
+++ src/pages/create-account-page-invalid-scenarios/create.account.page.invalid.scenarios.mjs
@@ -0,0 +1,12 @@
+"use strict"
+
+import {BasePage} from "../utilities/base.page.mjs";
+
+class CreateAccountPageInvalidScenarios extends BasePage{
+
+    constructor(page) {
+        super(page);
+    }
+
+}
+export {CreateAccountPageInvalidScenarios};
