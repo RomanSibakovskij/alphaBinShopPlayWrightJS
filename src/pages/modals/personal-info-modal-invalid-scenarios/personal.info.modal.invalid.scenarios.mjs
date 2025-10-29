3b4c2de6464e069ef98723564335b77b720b6bc7
RomanSibakovskij <benutzer373@gmail.com>
2025-10-29 13:55:14 +0200

    Add relevant web elements for PersonalInfoModalInvalidScenarios utility sheet

--- src/pages/modals/personal-info-modal-invalid-scenarios/personal.info.modal.invalid.scenarios.mjs
+++ src/pages/modals/personal-info-modal-invalid-scenarios/personal.info.modal.invalid.scenarios.mjs
@@ -6,6 +6,12 @@ class PersonalInfoModalInvalidScenarios extends BasePage{
 
     constructor(page) {
         super(page);
+
+        //relevant web elements
+        this._personalInfoModalFirstNameInputField = page.locator("//input[@data-testid='my-profile-firstname-input']");
+        this._personalInfoModalLastNameInputField = page.locator("//input[@data-testid='my-profile-lastname-input']");
+        this._personalInfoModalEmailInputField = page.locator("//input[@data-testid='my-profile-email-input']");
+
     }
 
 }
