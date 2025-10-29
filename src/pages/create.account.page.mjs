ae600e51207a684d845b6bbc0fa5eeac59ef0f2f
RomanSibakovskij <benutzer373@gmail.com>
2025-10-29 07:57:20 +0200

    Add empty CreateAccountPage sheet

---
+++ src/pages/create.account.page.mjs
@@ -0,0 +1,14 @@
+"use strict"
+
+import {BasePage} from "./utilities/base.page.mjs";
+import {TestDataGenerator} from "./utilities/test.data.generator.mjs";
+import {Logger} from "./utilities/logger.mjs";
+
+class CreateAccountPage extends BasePage{
+
+    constructor(page) {
+        super(page);
+    }
+
+}
+export {CreateAccountPage};
