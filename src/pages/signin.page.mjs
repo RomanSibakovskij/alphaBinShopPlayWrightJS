7d76d3529d1be41a7f0d76cca5c34060eb1ed7e3
RomanSibakovskij <benutzer373@gmail.com>
2025-10-29 07:31:13 +0200

    Add empty SignInPage sheet

---
+++ src/pages/signin.page.mjs
@@ -0,0 +1,12 @@
+"use strict"
+
+import {BasePage} from "./utilities/base.page.mjs";
+
+class SignInPage extends BasePage{
+
+    constructor(page) {
+        super(page);
+    }
+
+}
+export {SignInPage};
