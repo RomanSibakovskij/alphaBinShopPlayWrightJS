d9355baa0d5ad5e39a21be6543f44546cd7a8804
RomanSibakovskij <benutzer373@gmail.com>
2025-10-29 13:35:42 +0200

    Add empty PersonalInfoModal sheet

---
+++ src/pages/modals/personal.info.modal.mjs
@@ -0,0 +1,12 @@
+"use strict"
+
+import {BasePage} from "../utilities/base.page.mjs";
+
+class PersonalInfoModal extends BasePage{
+
+    constructor(page) {
+        super(page);
+    }
+
+}
+export {PersonalInfoModal};
