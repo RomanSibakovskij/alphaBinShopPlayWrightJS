a00d0d5e0d8ac05691d8355eb923a3e65a28e543
RomanSibakovskij <benutzer373@gmail.com>
2025-10-29 07:52:40 +0200

    Add empty TestDataGenerator utility sheet

---
+++ src/pages/utilities/test.data.generator.mjs
@@ -0,0 +1,12 @@
+"use strict";
+
+import {BasePage} from "./base.page.mjs";
+
+class TestDataGenerator extends BasePage{
+
+    constructor(page) {
+        super(page);
+    }
+
+}
+export {TestDataGenerator};
