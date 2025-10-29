ac54846226edc4fbfb6780c4e855c8ce8fdafb0d
RomanSibakovskij <benutzer373@gmail.com>
2025-10-29 07:23:30 +0200

    Add empty HomePage sheet

---
+++ src/pages/home.page.mjs
@@ -0,0 +1,12 @@
+"use strict"
+
+import {BasePage} from "./utilities/base.page.mjs";
+
+class HomePage extends BasePage{
+
+    constructor(page) {
+        super(page);
+    }
+
+}
+export {HomePage};
