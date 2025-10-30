356a0e3ee2f422260a5a1edd696f6beb958980c9
RomanSibakovskij <benutzer373@gmail.com>
2025-10-30 06:39:05 +0200

    Add relevant web elements for AddNewAddressModalInvalidScenarios utility sheet

--- src/pages/modals/add-new-address-modal-invalid-scenarios/add.new.address.modal.invalid.scenarios.mjs
+++ src/pages/modals/add-new-address-modal-invalid-scenarios/add.new.address.modal.invalid.scenarios.mjs
@@ -6,7 +6,19 @@ class AddNewAddressModalInvalidScenarios extends BasePage{
 
     constructor(page) {
         super(page);
+
+        //relevant web elements
+        this._addNewAddressModalFullNameInputField = page.locator("//input[@data-testid='first-name-input']");
+        this._addNewAddressModalEmailInputField = page.locator("//input[@data-testid='email-input']");
+        this._addNewAddressModalStreetInputField = page.locator("//input[@data-testid='street-address-input']");
+        this._addNewAddressModalCityInputField = page.locator("//input[@data-testid='city-input']");
+        this._addNewAddressModalStateInputField = page.locator("//input[@data-testid='state-input']");
+        this._addNewAddressModalCountryInputField = page.locator("//input[@data-testid='country-input']");
+        this._addNewAddressModalPostCodeInputField = page.locator("//input[@data-testid='zip-code-input']");
+
     }
+
+    
 
 }
 export {AddNewAddressModalInvalidScenarios};
