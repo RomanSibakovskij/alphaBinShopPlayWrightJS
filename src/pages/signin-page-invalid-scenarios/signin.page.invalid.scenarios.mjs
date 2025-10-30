e118b2016668609ae4d67a1fc247bcb2372ed227
RomanSibakovskij <benutzer373@gmail.com>
2025-10-30 13:33:30 +0200

    Add empty SignInPageInvalidScenarios utility sheet

---
+++ src/pages/signin-page-invalid-scenarios/signin.page.invalid.scenarios.mjs
@@ -0,0 +1,12 @@
+"use strict"
+
+import {BasePage} from "../utilities/base.page.mjs";
+
+class SignInPageInvalidScenarios extends BasePage{
+
+    constructor(page) {
+        super(page);
+    }
+
+}
+export {SignInPageInvalidScenarios};
