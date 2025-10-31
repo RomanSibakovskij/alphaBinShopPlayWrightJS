9c3853f15e9c3dcc64f7850afed5a795444af11b
RomanSibakovskij <benutzer373@gmail.com>
2025-10-31 07:47:50 +0200

    Add empty AddReviewModalInvalidScenarios utility sheet

---
+++ src/pages/modals/add-review-modal-invalid-scenarios/add.review.moda.invalid.scenarios.mjs
@@ -0,0 +1,12 @@
+"use strict"
+
+import {BasePage} from "../../utilities/base.page.mjs";
+
+class AddReviewModalInvalidScenarios extends BasePage{
+
+    constructor(page) {
+        super(page);
+    }
+
+}
+export {AddReviewModalInvalidScenarios};
