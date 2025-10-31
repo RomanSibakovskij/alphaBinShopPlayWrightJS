99cdd3e880264adec3144aede9458d64b471cff0
RomanSibakovskij <benutzer373@gmail.com>
2025-10-31 07:48:42 +0200

    Add relevant web elements for AddReviewModalInvalidScenarios utility sheet

--- src/pages/modals/add-review-modal-invalid-scenarios/add.review.moda.invalid.scenarios.mjs
+++ src/pages/modals/add-review-modal-invalid-scenarios/add.review.moda.invalid.scenarios.mjs
@@ -6,6 +6,13 @@ class AddReviewModalInvalidScenarios extends BasePage{
 
     constructor(page) {
         super(page);
+
+        //relevant web elements
+        this._addReviewModalYourNameInputField = page.locator("//input[@data-testid='review-form-name-input']");
+        this._addReviewModalYourEmailInputField = page.locator("//input[@data-testid='review-form-email-input']");
+        this._addReviewModalReviewTitleInputField = page.locator("//input[@data-testid='review-form-title-input']");
+        this._addReviewModalOpinionTextarea = page.locator("//textarea[@data-testid='review-form-review-input']");
+
     }
 
 }
