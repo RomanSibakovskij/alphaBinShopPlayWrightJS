ac7acbfde4e949ec95c8a9dbbca12cbd95b81d8a
RomanSibakovskij <benutzer373@gmail.com>
2025-10-31 05:51:34 +0200

    Add empty WishlistDashboardPage sheet

---
+++ src/pages/wishlist.dashboard.page.mjs
@@ -0,0 +1,12 @@
+"use strict"
+
+import {BasePage} from "./utilities/base.page.mjs";
+
+class WishlistDashboardPage extends BasePage{
+
+    constructor(page) {
+        super(page);
+    }
+
+}
+export {WishlistDashboardPage};
