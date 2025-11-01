d8edb197a3439a522a18f1994ce2b1301e6fba2d
RomanSibakovskij <benutzer373@gmail.com>
2025-11-01 06:01:18 +0200

    Add empty CheckoutPage sheet

---
+++ src/pages/checkout.page.mjs
@@ -0,0 +1,12 @@
+"use strict"
+
+import {BasePage} from "./utilities/base.page.mjs";
+
+class CheckoutPage extends BasePage{
+
+    constructor(page) {
+        super(page);
+    }
+
+}
+export {CheckoutPage};
