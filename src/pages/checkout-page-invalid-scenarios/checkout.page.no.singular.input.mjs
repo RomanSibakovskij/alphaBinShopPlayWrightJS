cc2a40ff743920c8c803ac9164bcf04c2e2110f5
RomanSibakovskij <benutzer373@gmail.com>
2025-11-01 06:54:18 +0200

    Add empty CheckoutPageNoSingularInput utility sheet

---
+++ src/pages/checkout-page-invalid-scenarios/checkout.page.no.singular.input.mjs
@@ -0,0 +1,12 @@
+"use strict"
+
+import {BasePage} from "../utilities/base.page.mjs";
+
+class CheckoutPageNoSingularInput extends BasePage{
+
+    constructor(page) {
+        super(page);
+    }
+
+}
+export {CheckoutPageNoSingularInput};
