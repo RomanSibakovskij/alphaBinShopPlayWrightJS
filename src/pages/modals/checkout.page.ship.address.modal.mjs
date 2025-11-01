0a61bd2b8d9e64db1a94e4f192c9dd81a440d44a
RomanSibakovskij <benutzer373@gmail.com>
2025-11-01 06:08:40 +0200

    Add empty CheckoutPageShipAddressModal sheet

---
+++ src/pages/modals/checkout.page.ship.address.modal.mjs
@@ -0,0 +1,12 @@
+"use strict"
+
+import {BasePage} from "../utilities/base.page.mjs";
+
+class CheckoutPageShipAddressModal extends BasePage{
+
+    constructor(page) {
+        super(page);
+    }
+
+}
+export {CheckoutPageShipAddressModal};
