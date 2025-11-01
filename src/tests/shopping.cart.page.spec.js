9bac343af1ed1a06dcba9cd7d97110cb99aae2b0
RomanSibakovskij <benutzer373@gmail.com>
2025-11-01 05:34:11 +0200

    Add All Products Dashboard Page Single Product To Checkout Test (as a registered user) method

--- src/tests/shopping.cart.page.spec.js
+++ src/tests/shopping.cart.page.spec.js
@@ -146,4 +146,22 @@ test.describe("Add All Products Dashboard Page Single Product To Checkout Tests"
         await testMethods.addProductToCheckoutTest(page);
     });
 
+    // Test 021a -> add all products dashboard page single product ("Apple iPad Air (2022, 5th Gen)") to check out test (as a registered user)
+    test("Add All Products Dashboard Page Single Product To Checkout Test (as a registered user)", async ({page}) => {
+        //launch the page
+        await page.goto('/');
+        //navigate to signup page test
+        await testMethods.navigateToSignUpPageTest(page);
+        //valid user account creation test
+        await testMethods.validUserAccountCreationTest(page)
+        //valid user login test
+        await testMethods.validUserLoginTest(page)
+        //valid add user address (without email) test
+        await testMethods.validAddUserAddressWithoutEmailTest(page);
+        //add all products dashboard page single product ("Apple iPad Air (2022, 5th Gen)") to cart test (as a registered user)
+        await testMethods.addSingleAllProductsDashPageProductToCartRegUserTest(page);
+        //add all products dashboard page single product ("Apple iPad Air (2022, 5th Gen)") to check out test (as a registered user)
+        await testMethods.addProductToCheckoutTest(page);
+    });
+
 });
