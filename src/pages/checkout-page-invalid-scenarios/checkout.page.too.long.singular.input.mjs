4da6eea4a23bdfa33d0a31002b01b88c2fb2581c
RomanSibakovskij <benutzer373@gmail.com>
2025-11-01 13:20:10 +0200

    Add empty CheckoutPageTooLongSingularInput utility sheet

---
+++ src/pages/checkout-page-invalid-scenarios/checkout.page.too.long.singular.input.mjs
@@ -0,0 +1,12 @@
+"use strict"
+
+import {BasePage} from "../utilities/base.page.mjs";
+
+class CheckoutPageTooLongSingularInput extends BasePage{
+
+    constructor(page) {
+        super(page);
+    }
+
+}
+export {CheckoutPageTooLongSingularInput};
