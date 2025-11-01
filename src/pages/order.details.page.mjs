1bc856a9a947f58baaae883cceac28cc61fefd42
RomanSibakovskij <benutzer373@gmail.com>
2025-11-01 06:12:25 +0200

    Add empty OrderDetailsPage sheet

---
+++ src/pages/order.details.page.mjs
@@ -0,0 +1,12 @@
+"use strict"
+
+import {BasePage} from "./utilities/base.page.mjs";
+
+class OrderDetailsPage extends BasePage{
+
+    constructor(page) {
+        super(page);
+    }
+
+}
+export {OrderDetailsPage};
