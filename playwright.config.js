090b95e49b0d85d2108ce5249b2ed0193307b634
RomanSibakovskij <benutzer373@gmail.com>
2025-11-02 12:48:49 +0200

    Set headless config to default

--- playwright.config.js
+++ playwright.config.js
@@ -28,8 +28,8 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://demo.alphabin.co/',
-    //headless: !!process.env.CI,
-    headless: true,
+    headless: !!process.env.CI,
+    //headless: false,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
