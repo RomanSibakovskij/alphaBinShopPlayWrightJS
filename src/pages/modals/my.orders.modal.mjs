fc68fc78b4974d165d3cece4d092e96e01b58ddf
RomanSibakovskij <benutzer373@gmail.com>
2025-11-02 06:49:45 +0200

    Add empty MyOrdersModal sheet

---
+++ src/pages/modals/my.orders.modal.mjs
@@ -0,0 +1,12 @@
+"use strict"
+
+import {BasePage} from "../utilities/base.page.mjs";
+
+class MyOrdersModal extends BasePage{
+
+    constructor(page) {
+        super(page);
+    }
+    
+}
+export {MyOrdersModal};
