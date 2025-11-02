7cc59344b7dacab14e84eb6a075a8bf975974d44
RomanSibakovskij <benutzer373@gmail.com>
2025-11-02 05:56:08 +0200

    Add relevant web elements for CheckoutPageInvalidSingularInput utility sheet

--- src/pages/checkout-page-invalid-scenarios/checkout.page.invalid.singular.input.mjs
+++ src/pages/checkout-page-invalid-scenarios/checkout.page.invalid.singular.input.mjs
@@ -6,6 +6,22 @@ class CheckoutPageInvalidSingularInput extends BasePage{
 
     constructor(page) {
         super(page);
+
+        //relevant web elements
+        this._checkoutPageShipAddressFullNameInputField = page.locator("//input[@data-testid='checkout-first-name-input']");
+        this._checkoutPageShipAddressEmailInputField = page.locator("//input[@data-testid='checkout-email-input']");
+        this._checkoutPageShipAddressCityInputField = page.locator("//input[@data-testid='checkout-city-input']");
+        this._checkoutPageShipAddressStateInputField = page.locator("//input[@data-testid='checkout-state-input']");
+        this._checkoutPageShipAddressInputField = page.locator("//input[@data-testid='checkout-street-input']");
+        this._checkoutPageShipAddressPostCodeInputField = page.locator("//input[@data-testid='checkout-zip-code-input']");
+        this._checkoutPageShipAddressCountryInputField = page.locator("//input[@data-testid='checkout-country-input']");
+
+        this._checkoutPagePayMethodCardNumberInputField = page.locator("//input[@data-testid='checkout-card-number-input']");
+        this._checkoutPagePayMethodCardholderInputField = page.locator("//input[@data-testid='checkout-cardholder-name-input']");
+        this._checkoutPagePayMethodExpDateMonthInputField = page.locator("//input[@data-testid='checkout-expiration-date-month-input']");
+        this._checkoutPagePayMethodExpDateYearInputField = page.locator("//input[@data-testid='checkout-expiration-date-year-input']");
+        this._checkoutPagePayMethodCVVInputField = page.locator("//input[@data-testid='checkout-cvv-input']");
+
     }
 
 }
